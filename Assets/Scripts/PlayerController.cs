8a803936a4ab48e84d4be10178cb44a2ec4f55b8
Nguyen Anh Duy <duy.nguyen280805@hcmut.edu.vn>
2025-10-14 02:14:03 +0700

    Basic Movement + CineMachine + Parallex Effect SetUp

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 7.5f;
+    [SerializeField] private float groundCheckRadius = 1f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        animator.SetBool("isRunning", moveInput != 0);
+        if (moveInput > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveInput < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    private void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+        
+    
+}
